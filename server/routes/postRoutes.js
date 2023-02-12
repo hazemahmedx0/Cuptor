c46bdf8ade5d1f8c23d8618bc8dc72014a8a4daa
moaaz <moaz.elsayed002@gmail.com>
2023-02-12 20:07:29 +0200

    Add cloudinary config

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import { v2 as cloudinary } from 'cloudinary'
 import * as dotenv from 'dotenv'
 
 import postController from '../controllers/postController.js'
@@ -7,6 +8,11 @@ dotenv.config()
 
 const router = express.Router()
 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+})
 router.route('/').get(postController.getPosts)
 
 router.route('/').post(postController.createPost)
