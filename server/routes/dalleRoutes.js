ad6bb980f187b7763798892e781cb8bb08a1d4f4
moaaz <moaz.elsayed002@gmail.com>
2023-02-12 18:00:01 +0200

    Add dalle routes

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -1,8 +1,11 @@
 import express from 'express'
 import * as dotenv from 'dotenv'
+import dalleController from '../controllers/dalleController.js'
 
 dotenv.config()
 
 const router = express.Router()
+
+router.route('/').post(dalleController.generateImage)
 
 export default router
